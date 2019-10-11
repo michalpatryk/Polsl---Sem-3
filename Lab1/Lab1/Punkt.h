c169d12342545378ee1dfe8401d21b5ae12452b2
Michał Urbańczyk <urbanczyka3k@gmail.com>
2019-10-11 13:00:42 +0200

    Punkt refactor

--- Lab1/Lab1/Punkt.h
+++ Lab1/Lab1/Punkt.h
@@ -4,41 +4,24 @@ class Punkt {
 	float x;
 	float y;
 public:
-	Punkt(Punkt &p);
-	Punkt(float x_ = 0, float y_ = 0);
-	void dodaj(Punkt &p);
-	void dodaj(Punkt *p);
+	Punkt(Punkt &p) { 
+		x = p.x; y = p.y; 
+	}
+	Punkt(float x_ = 0, float y_ = 0){
+		x = x_; y = y_;
+	}
+	void dodaj(Punkt &p){
+		x += p.x; y += p.y;
+	}
+	void dodaj(Punkt *p){
+		x += p->x; y += p->y;
+	}
 	std::string drukuj();
 };
 
-Punkt::Punkt(Punkt & p)
-{
-	x = p.x;
-	y = p.y;
-}
-
-Punkt::Punkt(float x_, float y_)
-{
-	x = x_;
-	y = y_;
-}
-
-void Punkt::dodaj(Punkt & p)
-{
-	x += p.x;
-	y += p.y;
-}
-
-void Punkt::dodaj(Punkt * p)
-{
-	x += p->x;
-	y += p->y;
-}
-
 std::string Punkt::drukuj()
 {
-	std::string output;
-	output = "Dane punktu:" + std::to_string(x) + "," + std::to_string(y) + "\n";
+	std::string output = "Dane punktu:" + std::to_string(x) + "," + std::to_string(y) + "\n";
 	return  output;
 }
 
