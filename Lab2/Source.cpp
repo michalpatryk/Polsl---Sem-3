167c4010a52b277cc91a03fde7361dc4b28c2198
Student <urbanczyka3k@gmail.com>
2019-10-22 17:02:33 +0200

    Main testing added

--- Lab2/Source.cpp
+++ Lab2/Source.cpp
@@ -8,12 +8,12 @@ int main() {
 	Punkt p2 = { 10,20 };
 	Punkt p3 = {};
 	p3 = p1 + p2;	//operator == i +
-	std::cout << p3;
+	std::cout << p3;	//operator <<
 	p3 += p1;		//operator +=
 	std::cout << p3;
 	if (p3 == p1)	//operator ==
 	{
-		std::cout << "p3 == p1" << std::endl;
+		std::cout << "p3 == p1" << std::endl;	
 	}
 	//testowanie tablicy
 	Tablica tab{ 5 };
@@ -22,18 +22,20 @@ int main() {
 	Tablica tab2{ x,y,10 };
 	Tablica tab3{ x, y, 10 };
 
-	if (tab == tab2) {
+	if (tab == tab2) {	//testowanie operaotra ==
 		std::cout << "tab1 == tab2" << std::endl;
 	}
 	else if (tab2 == tab3) {
 		std::cout << "tab2 == tab3" <<std::endl;
 	}
-	tab = tab2;
+	tab = tab2;	//testowanie operatorwa =
 	if (tab == tab2) {
 		std::cout << "After tab = tab2, tab == tab2" << std::endl;
 	}
 
-	std::cout << tab;
+	std::cout << tab <<std::endl;	//testowanie operatora <<
+
+	std::cout << tab[2];	//testowanie operatora []
 
 	return 0;
 }
