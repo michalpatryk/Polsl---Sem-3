098ca7f31d8d8b886879b3b32e08b66ada4f88e7
Student <urbanczyka3k@gmail.com>
2019-11-19 16:32:56 +0100

    Init3

--- Lab4/szach.cpp
+++ Lab4/szach.cpp
@@ -15,7 +15,16 @@ public:
 };
 class Goniec : public Figura
 {
-	// uzupe³niæ
+	Goniec() :Figura() {};
+	bool set(Szachownica *s_, int x_, int y_)
+	{
+		s = s_;
+		Figura::set(x_, y_, 'G');
+		// wywo³aæ set(int x,int y,Figura *f) z odpowiednim parametrem
+		return true;
+	}
+	void range();
+
 };
 
 
