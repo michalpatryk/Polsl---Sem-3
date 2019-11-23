90a97351d5ce0bd5884edc3ca0d7429410632b55
Michał Urbańczyk <urbanczyka3k@gmail.com>
2019-11-23 22:29:44 +0100

    Bishop working

--- Projekt/Projekt/Bishop.cpp
+++ Projekt/Projekt/Bishop.cpp
@@ -15,3 +15,39 @@ void Bishop::debug()
 	Piece::debug();
 	std::cout << "Bishop coord: " << this->coord.x << this->coord.y << std::endl;
 }
+
+void Bishop::createPossibleMoveTable()
+{
+	std::vector<std::vector<bool>> varr(8, std::vector<bool>(8, false));
+	possibleMoveTable = varr;
+	int x = this->coord.x - 1;
+	int y = this->coord.y - 1;
+	int checksum = -2;
+
+	int dx = 1, dy = 1;
+	for (int i = x, j = y; i < 8 && j < 8; i++, j++) {
+
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTable[j][i] = true;
+	}
+	checksum = -2;
+	dx = -1; dy = 1;
+	for (int i = x, j = y; i >= 0 && j < 8; i--, j++) {
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTable[j][i] = true;
+	}
+
+	checksum = -2;
+	dx = 1; dy = -1;
+	for (int i = x, j = y; i < 8 && j >= 0; i++, j--) {
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTable[j][i] = true;
+	}
+	checksum = -2;
+	dx = -1; dy = -1;
+	for (int i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTable[j][i] = true;
+	}
+	possibleMoveTable[y][x] = false;
+}
--- Projekt/Projekt/Bishop.h
+++ Projekt/Projekt/Bishop.h
@@ -10,5 +10,6 @@ public:
 	void debug();
 	PieceType returnType() { return piece; };
 	std::string getPieceFilename() { return "/Bishop.png"; };
+	void createPossibleMoveTable();
 };
 
