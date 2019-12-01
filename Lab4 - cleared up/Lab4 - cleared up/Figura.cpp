7576fc27b36f271be56511d95732ef8a0371adce
Michał Urbańczyk <urbanczyka3k@gmail.com>
2019-12-01 13:16:58 +0100

    Figura::set added

--- Lab4 - cleared up/Lab4 - cleared up/Figura.cpp
+++ Lab4 - cleared up/Lab4 - cleared up/Figura.cpp
@@ -1,9 +1,21 @@
 #include "Figura.h"
-//
-//
-//
-//Figura::Figura()
-//{
-//}
-//
-//
+#include <iostream>
+bool Figura::set(int x_, int y_, char z)
+{
+	if (x_ < 8 && x_>0 && y_ < 8 && y_>0 && s->sz[x_][y_] == nullptr) {
+		s->wiz[x_][y_] = z;
+		return true;
+	}
+	else return false;
+}
+
+bool Figura::set(int x_, int y_, Figura * f)
+{
+	if (x_ < 8 && x_>0 && y_ < 8 && y_>0 && s->sz[x_][y_] == nullptr) {
+		s->sz[x_][y_] = f;
+		f->x = x_;
+		f->y = y_;
+		return true;
+	}
+	else return false;
+}
