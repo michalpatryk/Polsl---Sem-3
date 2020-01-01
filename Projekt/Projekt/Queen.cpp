9ca0117d07c00195c83c3c120c5363917b76fede
Michał Urbańczyk <urbanczyka3k@gmail.com>
2020-01-01 18:49:25 +0100

    queen MV

--- Projekt/Projekt/Queen.cpp
+++ Projekt/Projekt/Queen.cpp
@@ -62,3 +62,65 @@ void Queen::createPossibleMoveTable()
 	}
 	possibleMoveTable[y][x] = false;
 }
+
+void Queen::createPossibleMoveTableMV()
+{
+	MicroVector<MicroVector<bool>> varr(8);
+	for (int i = 0; i < 8; i++) {
+		varr[i].clearAndSetSize(8);
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			varr[i][j] = false;
+		}
+	}
+	//bool init val is true
+	possibleMoveTableMV = varr;
+
+	int x = this->coord.x - 1;
+	int y = this->coord.y - 1;
+	int checksum = -2;
+	for (int i = x; i < 8; i++) {//go through horizontal -->
+		checksum += costTable[y][i];
+		if (checksum < 2) possibleMoveTableMV[y][i] = true;
+	}
+	checksum = -2;
+	for (int i = x; i >= 0; i--) {//go through horizontal <--
+		checksum += costTable[y][i];
+		if (checksum < 2) possibleMoveTableMV[y][i] = true;
+	}
+	checksum = -2;
+	for (int i = y; i < 8; i++) {//go through horizontal down
+		checksum += costTable[i][x];
+		if (checksum < 2) possibleMoveTableMV[i][x] = true;
+	}
+	checksum = -2;
+	for (int i = y; i >= 0; i--) {//go through horizontal up
+		checksum += costTable[i][x];
+		if (checksum < 2) possibleMoveTableMV[i][x] = true;
+	}
+
+	checksum = -2;
+	for (int i = x, j = y; i < 8 && j < 8; i++, j++) {
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTableMV[j][i] = true;
+	}
+	checksum = -2;
+	for (int i = x, j = y; i >= 0 && j < 8; i--, j++) {
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTableMV[j][i] = true;
+	}
+	checksum = -2;
+	for (int i = x, j = y; i < 8 && j >= 0; i++, j--) {
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTableMV[j][i] = true;
+	}
+	checksum = -2;
+	for (int i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+		checksum += costTable[j][i];
+		if (checksum < 2) possibleMoveTableMV[j][i] = true;
+	}
+	possibleMoveTableMV[y][x] = false;
+
+
+}
--- Projekt/Projekt/Queen.h
+++ Projekt/Projekt/Queen.h
@@ -11,5 +11,6 @@ public:
 	PieceType returnType() { return piece; };
 	std::string getPieceFilename() { return "/Queen.png"; };
 	void createPossibleMoveTable();
+	void createPossibleMoveTableMV();
 };
 
